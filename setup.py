00e774bab503ef641590b189189d7415f0b88f49
Farkas Martin <martinordasfarkas2004@gmail.com>
2025-03-27 19:05:04 +0100

    Javított setup.py package név

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 from setuptools import find_packages, setup
-<<<<<<< HEAD
 
 package_name = 'sun_drawer_4'
-=======
-from glob import glob
-import os
-
-package_name = 'ros2_py_template'
->>>>>>> 605dc6b60db023bf22ae5e3a7a90baf78b7b9c93
 
 setup(
-    name=package_name,
+    name='sun_drawer_4',
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-<<<<<<< HEAD
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -28,24 +20,8 @@ setup(
     tests_require=['pytest'],
 entry_points={
     'console_scripts': [
-        'draw_sun_3 = sun_drawer_4.draw_sun_4:main',
+        'draw_sun_3 = sun_drawer_4.draw_sun_3:main',
     ],
 },
 
-=======
-        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')), 
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='todo',
-    maintainer_email='todo@todo.com',
-    description='TODO: Package description',
-    license='GNU General Public License v3.0',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            # 'control_vehicle = ros2_py_template.control_vehicle:main',
-        ],
-    },
->>>>>>> 605dc6b60db023bf22ae5e3a7a90baf78b7b9c93
 )
